0ddab8ba718de5660e7747146a7df26878ec4f18
UnCript3d <sksrdier@gmail.com>
2025-04-17 17:48:05 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,14 @@
+let x =1;
+while (x<=100){
+    if (x%15===0){
+        console.log("Fizzbuzz");}
+    else if (x%3===0){
+        console.log("Fizz");}
+    else if (x%5===0){
+        console.log("Buzz");}
+    else {
+        console.log(x)
+    }
+     x+=1; 
+
+}
